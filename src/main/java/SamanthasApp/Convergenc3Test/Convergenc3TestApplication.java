3676d19e49dc6ced2be83bebe6c931e4bc3cc819
Sphelele <smakau021@student.wethinkcode.co.za>
2023-02-01 09:12:14 +0200

    springBoot project

---
+++ src/main/java/SamanthasApp/Convergenc3Test/Convergenc3TestApplication.java
@@ -0,0 +1,13 @@
+package SamanthasApp.Convergenc3Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Convergenc3TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Convergenc3TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SamanthasApp/Convergenc3Test/Convergenc3TestApplicationTests.java
@@ -0,0 +1,13 @@
+package SamanthasApp.Convergenc3Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Convergenc3TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
